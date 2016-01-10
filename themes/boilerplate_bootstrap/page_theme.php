bf9a0eaaef835ce81ae820b491098029cc273fa4
Rgeelen <rikgeelen@gmail.com>
2016-01-10 09:40:19 +0100

    added responsive images

--- themes/boilerplate_bootstrap/page_theme.php
+++ themes/boilerplate_bootstrap/page_theme.php
@@ -7,6 +7,13 @@ use Concrete\Core\Page\Theme\Theme;
 class PageTheme extends Theme{
 
 	/** 
+	 * Set bootstrap as grid frame work
+	 * Docs: http://goo.gl/GdvAr7
+	 */
+	
+	protected $pThemeGridFrameworkHandle = 'bootstrap3';
+
+	/** 
 	 * Register theme assets
 	 * Docs: http://goo.gl/JjCS6c
 	 */
@@ -14,17 +21,24 @@ class PageTheme extends Theme{
 	public function registerAssets(){
 		$this->providesAsset('css', 'bootstrap/*');
 		
-		
 		$this->requireAsset('javascript', 'jquery');
+		$this->requireAsset('javascript', 'picturefill');
+		
 	}
 
-
-	/** 
-	 * Set bootstrap as grid frame work
-	 * Docs: http://goo.gl/GdvAr7
+	/*
+	 * Create responsive images
+	 * Thumbnails need to be configured via settings
+	 * in the dashboard
 	 */
-
-	protected $pThemeGridFrameworkHandle = 'bootstrap3';
+	public function getThemeResponsiveImageMap()
+    {
+        return array(
+            'large' => '900px',
+            'medium' => '768px',
+            'small' => '0',
+        );
+    }
 
 
 } //end of class extend
